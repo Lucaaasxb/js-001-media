c1bb3497db8886436aaa4f91dfb97feb6dc42ea3
Lucaaasxb <lucasbriansilvafagundes@gmail.com>
2025-06-03 19:25:09 -0300

    update

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 let n1 = 7, n2 = 5.5, n3 = 2, n4 = 4
 let media = (n1 + n2 +n3 +n4) /4
 console.log("a média é:", media.toFixed(2))
+if (media >= 6) {
+    console.log("Aprovado")
+}else  {
+    console.log("Recuperação")
+}
